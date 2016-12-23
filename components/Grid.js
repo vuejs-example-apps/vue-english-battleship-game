52c0b790fbdc061fefebdfa316e4af35ab45a765
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-12-23 04:21:24 +0300

    ship placement; ship overlapping control [wip]

--- components/Grid.js
+++ components/Grid.js
@@ -156,12 +156,138 @@ var Grid = Vue.component('Grid', function (resolve, reject) {
                 _check_placement_possibility: function (ship, cell_name) {
                     var row = cell_name.charCodeAt(0) - 'a'.charCodeAt(0);
                     var col = parseInt(cell_name.slice(1)) - 1;
+                    ship.position = cell_name;
+                    console.log('_check_placement_possibility', row, col, ship.size);
+                    
                     // can't position next to grid walls
-                    if (row == 0 || col == 0 || row == 9 || col == 9) return false;
-                    // can't position if not fitting into grid
-                    if (!ship.is_vertical && col + ship.size > 9) return false;
-                    if (ship.is_vertical && row + ship.size > 9) return false;
-                    return true; // TODO check other ships intersection
+                    if (row == 0 || col == 0 || row == 9 || col == 9) {
+                        console.log('_check_placement_possibility: can\'t position next to grid walls');
+                        return false;
+                    }
+                    
+                    // can't position if not fitting into grid cols / rows
+                    if (!ship.is_vertical && col + ship.size > 9) {
+                        console.log('_check_placement_possibility: can\'t position if not fitting into grid cols');
+                        return false;
+                    }
+                    if (ship.is_vertical && row + ship.size > 9) {
+                        console.log('_check_placement_possibility: can\'t position if not fitting into grid rows');
+                        return false;
+                    }
+
+                    // can't intersect with other ships
+                    for (i in this.ships) {
+                        var other_ship = this.ships[i];                        
+                        var other_ship_row = other_ship.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                        var other_ship_col = parseInt(other_ship.position.slice(1)) - 1;
+                        
+                        console.log('- checking intersection with', other_ship_row, other_ship_col, other_ship.size);
+                        
+                        if (other_ship.position == cell_name && other_ship.size == ship.size ) {
+                            // TODO: better same-ship identification
+                            console.log('   identificated as the same ship, skipping');
+                            continue;
+                        }
+                        if (ship.is_vertical) {
+                            if (other_ship.is_vertical) {
+                                console.log('   both vertical');
+                                if (this._check_ships_overlap_or_touch(ship, other_ship)) {
+                                    return false;
+                                }
+                            } else {
+                                console.log('   maybe crossing');
+                                if (this._check_ships_cross_or_touch(ship, other_ship)) {
+                                    return false;
+                                }
+                            }
+                        } else {
+                            if (!other_ship.is_vertical) {
+                                console.log('   both horizontal');
+                                if (this._check_ships_overlap_or_touch(ship, other_ship)) {
+                                    return false;
+                                }
+                            } else {
+                                console.log('   maybe crossing');
+                                if (this._check_ships_cross_or_touch(ship, other_ship)) {
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    return true;
+                },
+
+                _check_ships_overlap_or_touch: function (ship1, ship2) {
+                    if (ship1.is_vertical) {                        
+                        // if both ships are vertical, they can't overlap if not sharing the same/adjacent columns
+                        var ship1_col = parseInt(ship1.position.slice(1)) - 1;
+                        var ship2_col = parseInt(ship2.position.slice(1)) - 1;
+                        console.log(' * both vertical', ship1_col, ship2_col, Math.abs(ship1_col-ship2_col))
+                        if (Math.abs(ship1_col-ship2_col) > 1) {
+                            console.log(' -> not sharing nor touching the column, leaving');
+                            return false;
+                        }
+                        var min_ship = this._min_row_ship(ship1, ship2)
+                        var max_ship = this._max_row_ship(ship1, ship2)
+                        var min_ship_row = min_ship.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                        var max_ship_row = max_ship.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                        console.log(' : ', min_ship_row, min_ship_row + min_ship.size, max_ship_row);
+                        console.log(' -> ', min_ship_row + min_ship.size >= max_ship_row);
+                        return min_ship_row + min_ship.size >= max_ship_row;
+                    } else {
+                        // if both ships are horizontal, they can't overlap if not sharing the same/adjacent raws
+                        var ship1_row = ship1.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                        var ship2_row = ship2.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                        console.log(' * both horizontal', ship1_row, ship2_row, Math.abs(ship1_row-ship2_row))
+                        if (Math.abs(ship1_row - ship2_row) > 1) {
+                            console.log(' -> not sharing nor touching the row, leaving');
+                            return false;
+                        }
+                        var min_ship = this._min_col_ship(ship1, ship2)
+                        var max_ship = this._max_col_ship(ship1, ship2)
+                        var min_ship_col = parseInt(min_ship.position.slice(1)) - 1;
+                        var max_ship_col = parseInt(max_ship.position.slice(1)) - 1;
+                        console.log(' : ', min_ship_col, min_ship_col + min_ship.size, max_ship_col);
+                        console.log(' -> ', min_ship_col + min_ship.size >= max_ship_col);
+                        return min_ship_col + min_ship.size >= max_ship_col;
+                    }
+                },
+
+                _check_ships_cross_or_touch: function (ship1, ship2) {
+                    if (ship1.is_vertical) {
+                        var ship1_row = ship1.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                        var ship2_row = ship2.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                        var ship1_col = parseInt(ship1.position.slice(1)) - 1;
+                        var ship2_col = parseInt(ship2.position.slice(1)) - 1;
+                        return (ship1_row <= ship2_row) && (ship2_row <= ship1_row + ship1.size) &&
+                               (ship2_col <= ship1_col) && (ship1_col <= ship2_col + ship2.size);
+                    } else {
+                        return this._check_ships_cross_or_touch(ship2, ship1);
+                    }
+                },
+
+                _max_row_ship: function (ship1, ship2) {
+                    var row1 = ship1.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                    var row2 = ship2.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                    return row1 > row2 ? ship1 : ship2;
+                },
+
+                _min_row_ship: function (ship1, ship2) {
+                    var row1 = ship1.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                    var row2 = ship2.position.charCodeAt(0) - 'a'.charCodeAt(0);
+                    return row1 < row2 ? ship1 : ship2;
+                },
+
+                _max_col_ship: function (ship1, ship2) {
+                    var col1 = parseInt(ship1.position.slice(1)) - 1;
+                    var col2 = parseInt(ship2.position.slice(1)) - 1;
+                    return col1 > col2 ? ship1 : ship2;
+                },
+
+                _min_col_ship: function (ship1, ship2) {
+                    var col1 = parseInt(ship1.position.slice(1)) - 1;
+                    var col2 = parseInt(ship2.position.slice(1)) - 1;
+                    return col1 < col2 ? ship1 : ship2;
                 },
 
                 _handle_my_shot: function (cell_name) {
